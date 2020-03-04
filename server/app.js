348a041b6bcf3dce66f6fa1dd6e7812472041e9c
Gaurav Pasarkar <gaurav.pasarkar@sell.do>
2020-03-04 10:34:04 +0530

    Intial commit for todos api

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import todoRouter from './routes/todos';
 
 var app = express();
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', todoRouter);
 
 export default app;
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,9 +1,24 @@
 import express from 'express'
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+const todos = [
+  {
+    id: 1,
+    name: "Build todo backend"
+  },
+  {
+    id: 2,
+    name: "Build todo ui"
+  },
+  {
+    id: 3,
+    name: "Build todo ops"
+  }
+];
+
+router.get('/todos', function(req, res, next) {
+  res.json(todos);
 });
 
 export default router;
