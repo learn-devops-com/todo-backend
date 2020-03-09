475ba4aa3a4045b206e4246c561921c8cad9d300
Gaurav Pasarkar <gaurav.pasarkar@sell.do>
2020-03-09 10:15:21 +0530

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 import logger from 'morgan';
 
 import todoRouter from './routes/todos';
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
